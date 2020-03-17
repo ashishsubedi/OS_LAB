0959b0b8a878ffe48e79cfc93a4fa3c85698213e
Ashish Subedi <ashishsubedi10@outlook.com>
2020-03-17 17:33:20 +0545

    Completed lab_01

--- lab_01.c
+++ lab_01.c
@@ -4,14 +4,19 @@
 
 int main(){
     int a = 0;
-    int i = fork();
-    if(i>=0){
+    int pid = fork();
+    if(pid==0){
         a = 2;
-        printf("Child %d\n",getppid());
-        exit(1);
+        printf("Child pid %d\n",getpid());
+        printf("Process of parent %d\n",getppid());
+        printf("value of a in child %d\n",a);
+        execlp("/bin/ls","ls",(char*)NULL);
+        exit(0);
     }else{
 
-        printf("Parent %d\n",i);
+        printf("Parent %d\n",pid);
+        printf("Value of a in parent %d\n",a);
+        exit(0);
     }
     return 0;
 }
